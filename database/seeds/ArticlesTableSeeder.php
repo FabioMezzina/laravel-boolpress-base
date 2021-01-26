3bb490b1db5f8adea67753a263e3b7e4f1621af5
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-01-26 15:18:22 +0100

    create seeder for articles table

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+use App\Article;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 20; $i++) {
+            $article = new Article();
+            $article->title = $faker->sentence(5);
+            $article->body = $faker->paragraphs(3, true);
+            $article->author = $faker->sentence(2);
+            $article->slug = Str::slug($article->title, '-');
+
+            $article->save();
+        }
+    }
+}
